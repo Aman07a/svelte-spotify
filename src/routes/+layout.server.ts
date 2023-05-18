fa560356c9e77ad7eaf5631a6157db432b1b5a1c
Aman07a <aman16.aa17@gmail.com>
2023-05-18 08:18:21 +0200

    Refreshing Expired Access Tokens

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,27 +1,35 @@
 import type { LayoutServerLoad } from './$types';
 import { SPOTIFY_BASE_URL } from '$env/static/private';
+import { redirect } from '@sveltejs/kit';
 
-export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
+export const load: LayoutServerLoad = async ({ cookies, fetch, url }) => {
 	const accessToken = cookies.get('access_token');
+	const refreshToken = cookies.get('refresh_token');
 	if (!accessToken) {
 		return {
 			user: null
 		};
 	}
 
-	console.log('+layout.server.ts access_token: ', accessToken);
-	console.log('Spotify_base_url: ', SPOTIFY_BASE_URL);
-
 	const profileRes = await fetch(`${SPOTIFY_BASE_URL}/me`, {
 		headers: {
 			Authorization: `Bearer ${accessToken}`
 		}
 	});
-
 	if (profileRes.ok) {
 		const profile: SpotifyApi.CurrentUsersProfileResponse = await profileRes.json();
 		return {
 			user: profile
+		};
+	}
+	if (profileRes.status === 401 && refreshToken) {
+		// refresh the token and try again
+		const refreshRes = await fetch('/api/auth/refresh');
+		if (refreshRes.ok) {
+			throw redirect(307, url.pathname);
+		}
+		return {
+			user: null
 		};
 	} else {
 		return {
