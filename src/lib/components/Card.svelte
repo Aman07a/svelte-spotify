15df340b5990f18cf019a7630f0e7b71c5eafe0e
Aman07a <aman16.aa17@gmail.com>
2023-05-21 15:54:20 +0200

    Creating The profile Page (Cont.)

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { Music } from 'lucide-svelte';
+
 	type AlbumType = SpotifyApi.AlbumObjectFull | SpotifyApi.AlbumObjectSimplified;
 	type PlaylistType = SpotifyApi.PlaylistObjectFull | SpotifyApi.PlaylistObjectSimplified;
 	type ArtistType = SpotifyApi.ArtistObjectFull;
+
 	export let item: AlbumType | PlaylistType | ArtistType;
+
+	const followersFormat = Intl.NumberFormat('en', { notation: 'compact' });
 </script>
 
 <div class="card {item.type}">
@@ -14,14 +18,15 @@
 			<Music aria-hidden="true" focusable="false" color="var(--light-gray)" />
 		</div>
 	{/if}
-	<h4 class="truncate-1">
-		<a href="/{item.type}/{item.id}" title={item.name}>{item.name}</a>
-	</h4>
+	<h4 class="truncate-1"><a href="/{item.type}/{item.id}" title={item.name}>{item.name}</a></h4>
 	{#if item.type === 'album'}
 		<p class="truncate-1">{item.artists.map((item) => item.name).join(', ')}</p>
 	{/if}
 	{#if item.type === 'playlist'}
 		<p class="truncate-2">{item.description}</p>
+	{/if}
+	{#if item.type === 'artist'}
+		<p class="truncate-1">{followersFormat.format(item.followers.total)} Followers</p>
 	{/if}
 </div>
 
@@ -33,6 +38,26 @@
 		border-radius: 4px;
 		transition: background 0.3s;
 		position: relative;
+		&.artist {
+			text-align: center;
+			padding: 30px 20px;
+			img,
+			.cover-placeholder {
+				width: 150px;
+				max-width: 100%;
+				border-radius: 100%;
+				box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+			}
+			h4 {
+				margin: 0;
+				text-align: center;
+				font-size: functions.toRem(18);
+				margin-bottom: 0.5em;
+			}
+			p {
+				margin: 0;
+			}
+		}
 		&:hover {
 			background-color: var(--medium-gray);
 			.cover-placeholder {
